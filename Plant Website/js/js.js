b23e40a0fb37c82e7129b6bb2ea87d4a5fa1d762
tj85967 <tj85967@yahoo.com.tw>
2023-09-23 10:50:28 +0800

    Plant Website

---
+++ Plant Website/js/js.js
@@ -0,0 +1,103 @@
+const swiper = new Swiper(".swiperTop", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: true,
+  delay: 2000,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperTwo = new Swiper(".swiperDown", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+
+  slidesPerView: 3,
+  spaceBetween: 50,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+AOS.init();
+
+const targetElement = document.getElementById('sectionTwo');
+targetElement.scrollIntoView;
+
+$("button").click(function(){
+  targetElement.scrollIntoView({
+    behavior: 'smooth'
+  });
+});
+
+$(document).ready(function () {
+  //按鈕點擊觸發   
+  $(".slideDown").click(function () {
+      $("#lightbox-1 , #lightbox_lightboxbg").fadeIn(300);
+      $("#lightbox-1").center();
+    });
+
+    $("a.lightbox_close").click(function () {
+      $("#lightbox_lightboxbg, .lightbox").fadeOut(300);
+    });
+    
+});
+
+(function ($) {
+  $.fn.extend({
+      center: function (options) {
+          var options = $.extend(
+              {
+                  // Default values
+                  inside: window, // element, center into window
+                  transition: 0, // millisecond, transition time
+                  minX: 0, // pixel, minimum left element value
+                  minY: 0, // pixel, minimum top element value
+                  withScrolling: false, // booleen, take care of the scrollbar (scrollTop)
+                  vertical: true, // booleen, center vertical
+                  horizontal: true, // booleen, center horizontal
+              },
+              options
+          );
+          return this.each(function () {
+              var props = {
+                  position: "fixed",
+              };
+              if (options.vertical) {
+                  var top =
+                      ($(options.inside).height() - $(this).outerHeight()) / 2;
+                  if (options.withScrolling)
+                      top += $(options.inside).scrollTop() || 0;
+                  top = top > options.minY ? top : options.minY;
+                  $.extend(props, {
+                      top: top + "px",
+                  });
+              }
+              if (options.horizontal) {
+                  var left =
+                      ($(options.inside).width() - $(this).outerWidth()) / 2;
+                  if (options.withScrolling)
+                      left += $(options.inside).scrollLeft() || 0;
+                  left = left > options.minX ? left : options.minX;
+                  $.extend(props, {
+                      left: left + "px",
+                  });
+              }
+              if (options.transition > 0)
+                  $(this).animate(props, options.transition);
+              else $(this).css(props);
+              return $(this);
+          });
+      },
+  });
+})(jQuery);
+
+
+
